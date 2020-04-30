2743689c2fe98984bcb90d56642fabf704ffb41a
Anthonia Carter <aoc29@cornell.edu>
2020-04-29 21:54:50 -0400

    Updated preprocessor.py to include function for Yow dataset.

---
+++ fairrecs/preprocessor.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import numpy as np
+
+class Preprocessor:
+    def preprocess_yow(self):
+        df = pd.read_csv('../datasets/yow_userstudy_raw.csv')
+
+        # remove NaN values
+        clds = df[df['classes']].notna()
+
+        # get 'people' topics
+        txds = clds[clds['classes'].str.contains("people")]
+
+        # select two most popular RSS IDs
+        top2_RSS_ID = txds.RSS_ID.value_counts().nlargest(2).to_dict()
+
+        top1 = list(top2_RSS_ID.keys())[0]
+        top2 = list(top2_RSS_ID.keys())[1]
+
+        top1_df = txds.loc[txds['RSS_ID'] == top1]
+        top2_df = txds.loc[txds['RSS_ID'] == top2]
+
+        final_df = pd.concat([top1_df, top2_df])
+
+        # normalize relevance to between 0 and 1, then add Gaussian noise,
+        final_df['relevant'] = (final_df['relevant'] / 5) + np.random.normal(0, 0.05, final_df['relevant'].count())
+
+        # then clip
+        final_df.loc[final_df['relevant'] < 0] = 0
+        final_df.loc[final_df['relevant'] > 1] = 1
+
+        return final_df
