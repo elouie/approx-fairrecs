f1062d87cfe1caa19e950bb3685a15b644c9d8bb
Erik Louie <etl43@cornell.edu>
2020-05-02 00:42:23 -0400

    Demographic Parity Constraint - Implement constraint in solver - An error occurs from cvxpy solver

--- fairrecs/dp_solver.py
+++ fairrecs/dp_solver.py
@@ -1,15 +1,24 @@
 from fairrecs.solver import Solver
+import numpy as np
 import cvxpy as cp
 
 
 class DPSolver(Solver):
-    def __init__(self, u):
-        super(u, self).__init__()
+    def __init__(self, u, g):
+        super().__init__(u)
+        uniq_ids = np.unique(g)
+        g1 = np.where(g == uniq_ids[0])
+        G1 = np.zeros(g.shape)
+        G1[g1] = 1
+        self.G1 = G1
+        g2 = np.where(g == uniq_ids[1])
+        G2 = np.zeros(g.shape)
+        G2[g2] = 1
+        self.G2 = G2
 
     def get_fair_constraint(self):
-        u = self.u
         v = self.v
         P = self.P
-        # TODO: Define f here
+        f = self.G1 / np.sum(self.G1) - self.G2 / np.sum(self.G2)
         fair_constraint = cp.matmul(cp.matmul(f.transpose(), P), v)
         self.constraints.append(fair_constraint)
